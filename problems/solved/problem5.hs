d59ed3d508250e53a0c9b4cc8c0f915c39d23d2d
Marco Pistilli <pistilli.mrc@gmail.com>
2023-04-22 21:05:14 +0100

    5 solved

---
+++ problems/solved/problem5.hs
@@ -0,0 +1,17 @@
+{-
+    # Problem: https://projecteuler.net/problem=5
+    2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+    What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+-}
+
+-- | Generalised Least Common Multiple
+glcm :: [Int] -> Int
+glcm [] = 1
+glcm [x] = x
+glcm [x, y] = lcm x y
+glcm (x:xs) = lcm x $ glcm xs
+
+res = glcm [1..20]
+{-
+    status : passed
+-}
