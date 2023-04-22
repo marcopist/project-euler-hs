f60318dcb075f47e8212e0416109f30ca60cafb4
Marco Pistilli <pistilli.mrc@gmail.com>
2023-04-22 17:43:07 +0100

    Passed 6 & fold restr

---
+++ problems/solved/problem6.hs
@@ -0,0 +1,14 @@
+{-
+    # Problem: https://projecteuler.net/problem=6
+    The sum of the squares of the first ten natural numbers is 1^2 + 2^2 + ... + 10^2 = 385
+    The square of the sum of the first ten natural numbers is (1+2+..10)^2 = 3025
+    Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 - 385 = 2640
+    Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+-}
+
+res :: Integer
+res = sum [1..100]^2 - sum (map (^2) [1..100])
+
+{-
+    status : passed
+-}
